1ba33cbb3d7255466c5115e59870a4274270c494
SadiKISLAR <sadi22kislar@gmail.com>
2022-11-19 11:58:57 +0300

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './pages/Home';
+import CreatePost from './pages/CreatePost';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/" > Home </Link>
+        <Link to="/createpost" > Create Post </Link>
+        <Link to="/login" > Login </Link>
+      </nav>
+
       <Routes>
-        <Route path="/" element={ } />
+        <Route path="/" element={<Home />} />
+        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/CreatePost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreatePost = () => {
+    return (
+        <div>Post</div>
+    )
+}
+
+export default CreatePost
