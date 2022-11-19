74c87c86ad67dfdce719bfdf41472cc5f6df0e0f
SadiKISLAR <sadi22kislar@gmail.com>
2022-11-19 11:45:47 +0300

    home created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={ } />
+      </Routes>
+    </Router>
   );
 }
 
