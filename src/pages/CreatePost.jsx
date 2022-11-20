e919923c1b3d773d08c14b9607da919cdb31ae98
SadiKISLAR <sadi22kislar@gmail.com>
2022-11-20 12:49:14 +0300

    post area

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const CreatePost = () => {
     return (
-        <div>Post</div>
+        <div className='createPostPage'>
+            <div className="cpContainer">
+                <h1>Create a post</h1>
+                <div className="inputGp">
+                    <label htmlFor="">Title:</label>
+                    <input placeholder='Title...' />
+                </div>
+                <div className="inputGp"></div>
+            </div>
+        </div>
     )
 }
 
