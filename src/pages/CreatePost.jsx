4a88e286110c95e9fba5b41e06315fdd55cc3987
SadiKISLAR <sadi22kislar@gmail.com>
2022-11-20 12:56:35 +0300

    text area done

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -6,12 +6,17 @@ const CreatePost = () => {
             <div className="cpContainer">
                 <h1>Create a post</h1>
                 <div className="inputGp">
-                    <label htmlFor="">Title:</label>
+                    <label>Title:</label>
                     <input placeholder='Title...' />
                 </div>
-                <div className="inputGp"></div>
+                <div className="inputGp">
+                    <label>Post:</label>
+                    <textarea placeholder='Post..' ></textarea>
+
+                </div>
+                <button> Submit Post </button>
             </div>
-        </div>
+        </div >
     )
 }
 
